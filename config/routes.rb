9b72f7b210bb0bae92371d97232531d194c12f6c
Aditya Sajwan <oca2602pg0001@edu.oca.ac.jp>
2026-07-24 00:25:39 +0000

    2026/07/24

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
+  # 新規商品
+  get 'products/new'
+  post 'products', to: 'products#create'  # 登録
+
+  # トップページ
   root to: "homes#top"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # 省略
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
