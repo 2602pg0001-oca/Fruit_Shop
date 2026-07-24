70123baf611dab914b904788fa707c78ee975826
Aditya Sajwan <oca2602pg0001@edu.oca.ac.jp>
2026-07-24 03:09:25 +0000

    2026/07/24 12:09

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,31 @@ class ProductsController < ApplicationController
   def index 
     @products = Product.all 
   end
-  
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    # 商品一覧にリダイレクト
+    redirect_to products_path
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  # 新規商品
-  get 'products/new'
-  post 'products', to: 'products#create'  # 登録
-
-  # 商品一覧
-  get 'products', to: 'products#index'
+  # 商品登録
+  resources :products
 
   # トップページ
   root to: "homes#top"
 
-  # 省略
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
 end
